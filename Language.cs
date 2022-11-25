7f70a268856cbd7f5f133ea07f389e33323d0970
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2022-11-25 12:21:59 +0530

    Update Language.cs

--- Language.cs
+++ Language.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Demo_Abstraction_in_OOP
 {
-    internal class Language
+     abstract internal class Language
     {
+       public abstract void LanguageName();
+    }
+
+    class English: Language
+    {
+        public override void LanguageName()
+        {
+            Console.WriteLine("name of the Language is English");
+        }
+    }
+
+    class Sanskrit : Language
+    {
+        public override void LanguageName()
+        {
+            Console.WriteLine("Language is Sanskrit");
+
+        }
+    }
+
+    class Telgu : Language
+    {
+        public override void LanguageName()
+        {
+            Console.WriteLine("Language is Telgu");
+        }
     }
 }
